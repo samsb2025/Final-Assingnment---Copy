2b8d98c4bd423c9c6c70591b721848954250cb96
samsb2025 <samtil2000b@gmail.com>
2025-11-22 08:00:50 +0300

    5th

--- backend/utils/redisClient.js
+++ backend/utils/redisClient.js
@@ -1,21 +1,18 @@
 import { createClient } from "redis";
 
 export const redisClient = createClient({
-  url:process.env.REDIS_URL || "redis://127.0.0.1:6379",
+  url: process.env.REDIS_URL || "redis://127.0.0.1:6379",
 });
 
 export const connectRedis = async () => {
   try {
-    
     redisClient.on("connect", () => console.log("Redis Client Connected"));
     redisClient.on("ready", () => console.log("Redis Client Ready"));
     redisClient.on("error", (err) => console.log("Redis Client Error", err));
-    
+
     await redisClient.connect();
     console.log("Connected to Redis");
   } catch (error) {
     console.error("Error connecting to Redis:", error);
   }
 };
-
-
